34858ffafdabd318ec1d8dcc19ce56f27e0328f0
Ben Gregory <JBGregoryPro@protonmail.com>
2021-07-20 15:50:44 -0500

    added evenodds

---
+++ evenodds.js
@@ -0,0 +1,17 @@
+const num = 7
+
+
+const evenOrOdd = () => {
+    
+        if (num % 2 === 0) {
+            return `even`
+        }
+        else {
+            return `odd`
+        }
+    }
+
+evenOrOdd()
+
+console.log(evenOrOdd())
+
